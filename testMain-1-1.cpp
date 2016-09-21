a186a016e6e882ef1c0d41a7e533e1b00b203811
Sivadius <davismartinjohn@gmail.com>
2016-09-21 12:33:53 +0930

    Basically nothing new

--- testMain-1-1.cpp
+++ testMain-1-1.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <string>
 #include "piece.h"
+#include <cstdlib>
 using namespace std;
 
 int main() {
 	Piece *p1;
 	p1 = new Piece(0);
 	p1->printColour();
+	cout << "8 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖" << endl;
+	cout << "7 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙" << endl;
+	cout << "6\n5\n4\n3" << endl;
+	// cout << "6 ■ □ ■ □ ■ □ ■ □" << endl;
+	// cout << "5 □ ■ □ ■ □ ■ □ ■" << endl;
+	// cout << "4 ■ □ ■ □ ■ □ ■ □" << endl;
+	// cout << "3 □ ■ □ ■ □ ■ □ ■" << endl;
+	// cout << "6 □ □ □ □ □ □ □ □" << endl;
+	// cout << "5 □ □ □ □ □ □ □ □" << endl;
+	// cout << "4 □ □ □ □ □ □ □ □" << endl;
+	// cout << "3 □ □ □ □ □ □ □ □" << endl;
+	cout <<	"2 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟" << endl;
+	cout << "1 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜" << endl;
+	cout << "  A B C D E F G H" << endl;
+	while(true) {
+		system("clear");
+		cout << "8 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖" << endl;
+		cout << "7 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙" << endl;
+		cout << "6\n5\n4\n3" << endl;
+		cout <<	"2 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟" << endl;
+		cout << "1 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜" << endl;
+		cout << "  A B C D E F G H" << endl;
+		string var;
+		cin >> var;
+		cout << var << endl;
+	}
 	delete p1;
 }
+
